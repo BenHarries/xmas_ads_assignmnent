3c7c5821781837770f3fcd8064d1869b92f2f0de
Ben Harries <Ben@Lukes-iMac-2.local>
2018-12-16 21:11:23 +0000

    completed

--- jgwp24_q1.py
+++ jgwp24_q1.py
@@ -8,14 +8,17 @@ def hash_quadratic(d):
     for k in d:
         i = ((6*k)+3)%19
         count = 0
+        
         while table[i] != "-":
+            i = ((((6*k)+3)%19)+ (count**2))%19
             count+=1
-            i = i+ (count**2)%19
             
-            if count == 19:
-                return table
-        table[i] = k
-    return table
+            if count == 20:
+                break
+        if table[i] == '-':
+            table[i] = k
+    return(table)
+    
 
 def hash_double(d):
     """given a list of d of integers returns a list of length 19
@@ -26,12 +29,17 @@ def hash_double(d):
     table = ["-"]*19
     for k in d:
         i = ((6*k)+3)%19
+        
         count = 0
         while table[i] != "-":
             count+=1
-            i = i + count*(11-(k%11)) 
+            i = (i + (11-(k%11)))%19
+            print
+            if count == 19:
+                return(table)
         table[i] = k
     return table
+
 
 def test_hq():
     assert hash_quadratic([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]) == [9, 6, 3, 19, 16, 13, 10, 7, 4, 1, 17, 14, 11, 8, 5, 2, 18, 15, 12]
@@ -45,5 +53,3 @@ def test_dh():
     assert hash_double([19,38,57,76,95,114,133,152,171,190,209,228,247,266,285,304,323,342,361]) == [304, 361, 266, 19, 76, 152, 228, 95, 171, 38, 114, 190, 57, 133, 209, 247, 285, 323, 342]
     assert hash_double([2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79]) == [47, 67, 3, 19, 53, 13, 29, 7, 23, 59, 17, 41, 11, 61, 5, 2, 37, 43, 31]
     print ("all tests passed")
-
-        
